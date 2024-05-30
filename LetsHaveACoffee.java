e8907e440fbfc7f8ccbce8de05ac5da680ca5b0f
nishap03 <nishayowan@gmail.com>
2024-05-30 09:13:59 +0000

    Create LetsHaveACoffee.java

---
+++ LetsHaveACoffee.java
@@ -0,0 +1,16 @@
+package com.lao.conditional_statements;
+
+public class LetsHaveACoffee {
+
+	boolean isCupEmpty = false;
+	public static void main(String[] args) {
+		LetsHaveACoffee coffee = new LetsHaveACoffee();
+		if(coffee.isCupEmpty) {
+			System.out.println("Fill the cup");
+		}
+		else {
+			System.out.println("Drink the coffee");
+		}
+	}
+
+}
